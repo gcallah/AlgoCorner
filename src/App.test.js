625df3ab8c29ab33b4678a62f182c7f3a5e51da4
Anupam Sushil <anupam.sushil1@gmail.com>
2022-11-16 08:02:00 -0500

    start of some logic unit tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import App from "./App";
+import OptionsPage from "./pages/Options";
 import ReactDOM from "react-dom";
 import { act } from "react-dom/test-utils";
-
+// following 5 are simple component rendering sucessfully unit tests
 it("renders without crashing", () => {
   act(() => {
     const div = document.createElement("div");
@@ -34,3 +35,19 @@ test("can select an algorithm", () => {
   expect(screen.getByRole("option", { name: "Binary Search Tree" }).selected);
   // expect(screen.getByRole('option', { name: 'Hash Table' }).selected).toBeInTheDocument();
 });
+
+// logic unit tests
+it("should show the correct questions for chosen algorithm", async () => {
+  let { user } = prep(<OptionsPage />)
+  let requiredRadioOption = screen.getByLabelText("radio-option-0")
+
+  expect(requiredRadioOption).toBeInvalid()
+
+  await user.click(requiredRadioOption)
+
+  expect(requiredRadioOption).toBeValid()
+})
+
+// another unit test to check the validity of the user inputs
+// another unit test if the user input is being passed into visualizations
+
